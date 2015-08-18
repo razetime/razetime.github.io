89eecf25a9dea9254591561e8ce569a8b4298e4a
RaghuAllTheTime <rraghu.11502@gmail.com>
2015-08-18 21:36:59 +0530

    Added background and few styles

--- js/index.js
+++ js/index.js
@@ -1,27 +1,31 @@
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $(this).addClass("disabled");
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+var main = function () {
+    
+    /* Push the body and the nav over by 285px over */
+    $('.icon-menu').click(function () {
+        $(this).addClass("disabled");
+        
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
 
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
+        $('body').animate({
+            left: "285px"
+        }, 200);
+    });
 
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.icon-menu').removeClass("disabled");
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
+    /* Then push them back */
+    $('.icon-close').click(function () {
+        $('.icon-menu').removeClass("disabled");
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+        $('.menu').animate({
+            left: "-285px"
+        }, 200);
+
+        $('body').animate({
+            left: "0px"
+        }, 200);
+    });
+    
 };
 
 
