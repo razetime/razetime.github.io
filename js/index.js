2118f70c971adde839b1a40b528ac9f58082e743
RaghuAllTheTime <rraghu.11502@gmail.com>
2015-08-28 22:24:13 +0530

    Added more homepage stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 var main = function () {
+    $('.jumbotron .container').hide();
+    $('.jumbotron .container').fadeIn(5000);
     
     /* Push the body and the nav over by 285px over */
     $('.icon-menu').click(function () {
