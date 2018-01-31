7f43657fad7a1812aa9437612949b85f871bd993
Razetime <rraghu.11502@gmail.com>
2018-01-31 06:24:47 +0530

    Added porjects mage, contact apge, minimal js for projects page. Small adjustments for mobile and faster loading

---
+++ js/projects.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#codin').click(function() {
+        $('.card').fadeOut();
+        $('.code').fadeIn();
+    });
+    $('#writin').click(function() {
+        $('.card').fadeOut();
+        $('.write').fadeIn();
+    });
+    $('#musin').click(function() {
+        $('.card').fadeOut();
+        $('.music').fadeIn();
+    });
+});
