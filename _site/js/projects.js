e89813e57725afd0f08062758e91bfd6e6d69cea
Razetime <rraghu.11502@gmail.com>
2018-02-01 19:07:57 +0530

    Conversion to Jekyll static page, major changes to files & base blog

---
+++ _site/js/projects.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#codin').click(function() {
+        $('.card').fadeOut();
+        $('.code').fadeIn();
+    });
+    $('#writin').click(function() {
+        $('.card').fadeOut();
+        $('.write').fadeIn();
+    });
+    $('#musin').click(function() {
+        $('.card').fadeOut();
+        $('.music').fadeIn();
+    });
+    $('#all').click(function() {
+        $('.card').fadeIn();
+    });
+});
